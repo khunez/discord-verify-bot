728dc0c170603d57ba4bc8c8c92853c6c1922e02
khunez <kjunezvgy@gmail.com>
2026-01-17 14:47:46 +0700

    Update web design

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// อนุญาตให้คนเข้าถึงไฟล์ Static ทั้งหมด (html, css, js) ที่วางไว้หน้าหลัก
+// บรรทัดนี้จะทำให้เมื่อเข้า /home ระบบจะไปดึง home/index.html มาให้เองค่ะ
 app.use(express.static(path.join(__dirname, '/')));
 
-// ตั้งค่าให้หน้าแรกเรียก index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// ส่งออก module สำหรับ Vercel
 module.exports = app;
