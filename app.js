e58851146ef3ec492e674141b87359294c93ec4c
khunez <kjunezvgy@gmail.com>
2026-01-17 14:34:58 +0700

    Update web design

--- app.js
+++ app.js
@@ -2,25 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// 1. ตั้งค่าให้ Express เข้าถึงไฟล์ในโฟลเดอร์ต่างๆ ได้ (สำคัญมากค่ะ)
+// อนุญาตให้เข้าถึงไฟล์ Static ทั้งหมดในหน้าหลัก (index.html, home.html, ฯลฯ)
 app.use(express.static(path.join(__dirname, '/')));
 
-// 2. จัดการ Route หลัก
+// ตั้งค่าหน้าแรกให้แสดง index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 3. จัดการ Route สำหรับหน้าอื่นๆ (ถ้าต้องการ URL แบบสวยๆ)
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home/home.html')); // แก้ชื่อไฟล์ให้ตรงกับที่คุณส่งมาคือ home.html
-});
-
-app.get('/fivem', (req, res) => {
-    res.sendFile(path.join(__dirname, 'fivem/fivem.html'));
-});
-
-app.get('/rules', (req, res) => {
-    res.sendFile(path.join(__dirname, 'rules/rules.html'));
-});
-
+// ส่งออกแอปเพื่อให้ Vercel นำไปรันต่อได้
 module.exports = app;
